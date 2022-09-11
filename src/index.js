153450c4fd3ea80fe6c68d8c06e27fe6f5c87e54
dawidmarek95x <dawid.marek95x@gmail.com>
2022-09-11 12:35:27 +0200

    Correction of the basename in BrowserRouter attribute

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const NotFound = React.lazy(() => import("./pages/NotFound/NotFound"));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter>
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<App />}>
